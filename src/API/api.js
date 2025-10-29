7e50bbb9de0fd2c0b7e414c4bcebde46a093631f
alejo <alejandrocardenas.dev@gmail.com>
2025-10-28 20:57:59 -0500

    feat(API): add centralized API endpoints configuration

---
+++ src/API/api.js
@@ -0,0 +1,18 @@
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const API = {
+  GET_ACCESS_TOKEN: `${API_BASE_URL}/token/`,
+
+  CREATE_USER: `${API_BASE_URL}/create-user/`,
+
+  CREATE_APPOINTMENT: `${API_BASE_URL}/create-appointment/`,
+  DELETE_APPOINTMENT: `${API_BASE_URL}/delete-appointment/`,
+  GET_CUSTOMER_APPOINTMENTS: `${API_BASE_URL}/get-customer/appointments/`,
+  GET_BARBER_APPOINTMENTS: `${API_BASE_URL}/get-barber-appointments/`,
+
+  GET_BARBERS: `${API_BASE_URL}/get-barbers/`,
+  GET_BARBERS_AVAILABLE_TIMES_SPECIFIC_DATE: `${API_BASE_URL}/get-barber/`,
+  EDIT_BARBER_SCHEDULE: `${API_BASE_URL}/edit-barber-schedule/`,
+};
+
+export default API;
