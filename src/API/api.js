cb1f4b82fa57c9cab1d80ddb9916cba71e6056fa
alejo <alejandrocardenas.dev@gmail.com>
2025-11-13 18:50:00 -0500

    feat(auth, API): add GetUser endpoint and its implementation in authContext

--- src/API/api.js
+++ src/API/api.js
@@ -3,7 +3,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const API = {
   GET_ACCESS_TOKEN: `${API_BASE_URL}/token/`,
 
-  CREATE_USER: `${API_BASE_URL}/create-user/`,
+  CREATE_USER: `${API_BASE_URL}/create-account/`,
 
   CREATE_APPOINTMENT: `${API_BASE_URL}/create-appointment/`,
   DELETE_APPOINTMENT: `${API_BASE_URL}/delete-appointment/`,
@@ -12,6 +12,8 @@ export const API = {
   GET_BARBERS: `${API_BASE_URL}/get-barbers/`,
   GET_BARBERS_AVAILABLE_TIMES_SPECIFIC_DATE: `${API_BASE_URL}/get-barber/`,
   EDIT_BARBER_SCHEDULE: `${API_BASE_URL}/edit-barber-schedule/`,
+
+  GET_USER: `${API_BASE_URL}/get-user/`
 };
 
 export default API;
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,13 +1,53 @@
 'use client'
 import API from "@/API/api"
 import { useRouter } from "next/navigation"
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 
 const AuthContext = createContext()
 
 export function AuthProvider ({ children }) {
+  const [user, setUser] = useState({})
   const [token, setToken] = useState('')
   const router = useRouter()
+
+  const fetch_user = async (access) => {
+    const res = await fetch(API.GET_USER, {
+      headers: { 'Authorization': `Bearer ${access}` }
+    })
+
+    if (!res.ok) {
+      console.log('error fetching userrrrrrrr');
+      return
+    }
+
+    const userData = await res.json()
+
+    if (userData.is_barber) {
+      const resBarber = await fetch(API.EDIT_BARBER_SCHEDULE, {
+        method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${access}`,
+          },
+        })
+
+      if (resBarber.ok) {
+        const dataBarber = await resBarber.json()
+        setUser(dataBarber)
+        console.log('barber user successsssssss', dataBarber);
+        router.push('/edit-schedules/')
+        return
+      }
+      console.log('Error fetching barber data')
+      return
+    }
+
+    else if (userData.is_customer) {
+      setUser(userData)
+      console.log('userrrr', userData);
+      router.push('/')
+    }
+  }
 
   const login = async (email, password) => {
     const res = await fetch(API.GET_ACCESS_TOKEN, {
@@ -18,16 +58,36 @@ export function AuthProvider ({ children }) {
 
     if (res.ok) {
       const data = await res.json()
-      console.log('data access:, ', data.access);
-      setToken(data.access)
-      router.push('/')
       localStorage.setItem('token', data.access)
+      setToken(data.access)
+      console.log('data access:, ', data.access);
+      await fetch_user(data.access)
       return true
     }
   }
 
+  useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+
+    const validateToken = async () => {
+      if (savedToken) {
+        setToken(savedToken);
+        try {
+          await fetch_user(savedToken)
+        } catch (err) {
+          console.error("Error validando token:", err)
+          localStorage.removeItem("token")
+          setToken(null)
+          setUser(null)
+        }
+      }
+    };
+
+    validateToken();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ token, login }}>
+    <AuthContext.Provider value={{ token, login, user }}>
       { children }
     </AuthContext.Provider >
   )
