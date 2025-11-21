20b2be17b6d8bedc72a9e5e2c04749a292079d69
alejo <alejandrocardenas.dev@gmail.com>
2025-11-21 14:22:20 -0500

    feat(loader): add global loader component

---
+++ src/components/loader.js
@@ -0,0 +1,15 @@
+export default function Loader () {
+  return (
+    <div className="flex justify-center items-center py-10 w-full h-screen ">
+      <div
+        className="
+          w-20 h-20
+          border-4 border-neutral-700
+          border-t-white
+          rounded-full
+          animate-spin
+        "
+      />
+    </div>
+  )
+}
