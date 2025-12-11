155449f0aa6ab01fcf14290c1c21b5c577de7fce
alejo <alejandrocardenas.dev@gmail.com>
2025-12-10 19:39:59 -0500

    refactor(layout, app): clean components

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -2,8 +2,6 @@ import Barbers from "@/components/barbers/barbers";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center">
-      <Barbers />
-    </div>
+    <Barbers />
   );
 }
