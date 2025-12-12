fccf064be6c3d59b3c611a1379d6f0120c516c33
alejo <alejandrocardenas.dev@gmail.com>
2025-12-12 17:55:46 -0500

    feat(appointment): add appointment confirmation component and its implementation

--- src/components/appointments/createAppointment.js
+++ src/components/appointments/createAppointment.js
@@ -1,77 +1,47 @@
 'use client'
 
 import { useState } from "react"
-import { FaCheckCircle } from "react-icons/fa";
-import API from "@/API/api"
-import { useAuthContext } from "@/context/authContext"
+import { FaCheckCircle, FaExclamationCircle, FaTimesCircle } from "react-icons/fa";
+
 import { useBarberContext } from "@/context/barberContext"
 import BarberDetail from "../barbers/barberDetail"
 import BarbersSchedules from "../barbers/barbersSchedules"
+import CreateConfirmation from "./createConfirmation";
 
 export default function CreateAppointmen () {
-  const [loading, setLoading] = useState(false)
   const [message, setMessage] = useState('')
-  const { token } = useAuthContext()
+  const [createAppointment, setCreateAppointment] = useState(false)
+  const { timeToCreateAppointment } = useBarberContext()
 
-  const {
-    timeToCreateAppointment,
-    barberToCreateAppointment,
-    dateToCreateAppointment,
-    reloadBarberSchedules,
-    setReloadBarberSchedules,
-    setTimeToCreateAppointment
-  } = useBarberContext()
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    setLoading(true)
-
-    const res = await fetch(API.CREATE_APPOINTMENT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        barber: barberToCreateAppointment,
-        appointment_date: dateToCreateAppointment,
-        appointment_start_time: timeToCreateAppointment,
-      })
-    })
-    const data = await res.json()
-
-    if (res.ok) {
-      setLoading(false)
-      setReloadBarberSchedules(!reloadBarberSchedules)
-      setTimeout(() => setMessage(''), 3000)
-      setTimeToCreateAppointment(null)
-
-    } else {
-      const error = Object.values(data).find(data => Array.isArray(data))?.[0]
-      setMessage(error)
-      setLoading(false)
-      setTimeToCreateAppointment(null)
-      setTimeout(() => setMessage(''), 3000)
-    }
+  const handleOpenModal = () => {
+    setCreateAppointment(!createAppointment)
   }
 
   return (
     <div className="flex flex-col items-center md:flex-row md:justify-center md:items-start gap-12 py-10">
 
       {message && (
-        <div className="
-          flex items-center gap-3
-          fixed bottom-6 right-6
-          px-5 py-3
-          rounded-2xl
-          bg-white text-black
-          shadow-[0_4px_15px_rgba(0,0,0,0.25)]
-          font-semibold
-          z-50
-          animate-fadeIn
-        ">
+        <div
+          className={`
+            flex items-center gap-3
+            fixed bottom-6 right-6
+            px-5 py-3
+            rounded-2xl
+            shadow-[0_4px_15px_rgba(0,0,0,0.25)]
+            font-semibold
+            z-50
+            animate-fadeIn
+            ${message.includes('successfully')
+              ? 'bg-white text-black'
+              : 'bg-red-600 text-white'
+            }
+          `}
+        >
           <span className="text-lg">
-            <FaCheckCircle />
+            {message.includes('successfully')
+              ? <FaCheckCircle />
+              : <FaExclamationCircle />
+            }
           </span>
           <span>{message}</span>
         </div>
@@ -85,7 +55,7 @@ export default function CreateAppointmen () {
 
         <div className="flex justify-end w-full">
           <button
-            onClick={handleSubmit}
+            onClick={handleOpenModal}
             disabled={!timeToCreateAppointment}
             className={`
               flex justify-center items-center w-[120px] py-2 rounded-2xl font-semibold transition-all
@@ -95,12 +65,18 @@ export default function CreateAppointmen () {
             `}
           >
             Create
-            {loading &&
-              <div className="h-4 w-4 ml-2 animate-spin rounded-full border-2 border-black border-t-transparent"></div>
-            }
           </button>
         </div>
       </div>
+
+      {createAppointment && (
+        <CreateConfirmation
+          setCreateAppointment={setCreateAppointment}
+          setMessage={setMessage}
+        />
+      )
+      }
+
     </div>
   )
 }
