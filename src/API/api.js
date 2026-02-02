64c7dd7a8c6b2d54ee983029986ca13eb9c7a7dd
alejo <alejandrocardenas.dev@gmail.com>
2026-02-02 14:45:12 -0500

    feat(API): rename endpoints

--- src/API/api.js
+++ src/API/api.js
@@ -3,17 +3,20 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const API = {
   GET_ACCESS_TOKEN: `${API_BASE_URL}/token/`,
 
-  CREATE_USER: `${API_BASE_URL}/create-account/`,
+  CREATE_USER: `${API_BASE_URL}/users/`,
 
-  CREATE_APPOINTMENT: `${API_BASE_URL}/create-appointment/`,
-  DELETE_APPOINTMENT: `${API_BASE_URL}/delete-appointment/`,
-  GET_APPOINTMENTS: `${API_BASE_URL}/get-appointments/`,
+  CREATE_APPOINTMENT: `${API_BASE_URL}/appointments/`,
+  DELETE_APPOINTMENT: `${API_BASE_URL}/appointment/`,
+  GET_APPOINTMENTS: `${API_BASE_URL}/appointments/`,
 
-  GET_BARBERS: `${API_BASE_URL}/get-barbers/`,
-  GET_BARBERS_AVAILABLE_TIMES_SPECIFIC_DATE: `${API_BASE_URL}/get-barber/`,
-  EDIT_BARBER_SCHEDULE: `${API_BASE_URL}/edit-barber-schedule/`,
+  GET_BARBERS: `${API_BASE_URL}/barbers/`,
+  GET_BARBERS_AVAILABLE_TIMES_SPECIFIC_DATE: `${API_BASE_URL}/barbers/`,
+  EDIT_BARBER_SCHEDULE: `${API_BASE_URL}/ barbers/me/schedule/`,
 
-  GET_USER: `${API_BASE_URL}/get-user/`
+  GET_USER: `${API_BASE_URL}/users/me`,
+
+  GET_SERVICES: `${API_BASE_URL}/services/`,
+
 };
 
 export default API;
